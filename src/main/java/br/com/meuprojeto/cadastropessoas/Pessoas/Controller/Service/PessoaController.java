d424c52e5595a9ef529cf3e3c2d71d178086c222
SergioCezar <sergioacezar@hotmail.com>
2026-03-17 16:19:11 -0300

    feat: Atualiza o controller com o mock de dados

--- src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaController.java
+++ src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaController.java
@@ -1,8 +1,6 @@
 package br.com.meuprojeto.cadastropessoas.Pessoas.Controller.Service;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -13,4 +11,33 @@ public class PessoaController {
         return "Primeira mensagem nessa rota";
     }
 
+    //Adicionar pessoa
+    @PostMapping("/criar")
+    public String criarPessoa(){
+        return "Pessoa criada com sucesso";
+    }
+
+    //Mostrar todas as pessoas
+    @GetMapping("/todos")
+    public String mostrarTodasAsPessoas(){
+        return "Mostrar pessoas";
+    }
+
+    //Mostrar pessoa por ID
+    @GetMapping("/todosID")
+    public String mostrarTodasAsPessoasPorID(){
+        return "Mostrar pessoas por id";
+    }
+
+    //Alterar dados das pessoas
+    @PutMapping("/alterarID")
+    public String alterarPessoasPorID(){
+        return "Alterar pessoas por id";
+    }
+
+    //Deletar pessoa
+    @DeleteMapping("/deletarID")
+    public String deletarPessoaPorID(){
+        return "Ninja deletado por ID ";
+    }
 }
