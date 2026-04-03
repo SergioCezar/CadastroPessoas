b6f0176c16b227d35b5339af6ff7a121fa8c25e6
SergioCezar <sergioacezar@hotmail.com>
2026-04-03 17:42:07 -0300

    chore: inclui a versao do DTO pela entidade e mapeia manualmente no pessoaMapper os caminhos entidade x DTO

--- src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaDTO.java
+++ src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaDTO.java
@@ -1,4 +1,21 @@
 package br.com.meuprojeto.cadastropessoas.Pessoas.Controller.Service;
 
+import br.com.meuprojeto.cadastropessoas.Tarefas.TarefasModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//Clone do model (entidade) para tirar a responsabilidade do model de conversar com outras camadas. Crucial para segurança.
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PessoaDTO {
-}
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String imgUrl;
+    private int idade;
+    private TarefasModel tarefas;
+    private String telefone;
+    }
--- src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaMapper.java
+++ src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaMapper.java
@@ -1,4 +1,37 @@
 package br.com.meuprojeto.cadastropessoas.Pessoas.Controller.Service;
 
+import org.springframework.stereotype.Component;
+
+//Relaciona o DTO com o Model (id do DTO --> id do Model) e vice-versa.
+@Component
 public class PessoaMapper {
+
+    public PessoaModel map(PessoaDTO pessoaDTO) {
+
+        PessoaModel pessoa = new PessoaModel();
+        pessoa.setId(pessoaDTO.getId());
+        pessoa.setNome(pessoaDTO.getNome());
+        pessoa.setEmail(pessoaDTO.getEmail());
+        pessoa.setIdade(pessoaDTO.getIdade());
+        pessoa.setImgUrl(pessoaDTO.getImgUrl());
+        pessoa.setTelefone(pessoaDTO.getTelefone());
+        pessoa.setTarefas(pessoaDTO.getTarefas());
+
+        return pessoa;
+    }
+
+    public PessoaDTO map(PessoaModel pessoaModel) {
+
+        PessoaDTO pessoaDTO = new PessoaDTO();
+        pessoaDTO.setId(pessoaModel.getId());
+        pessoaDTO.setNome(pessoaModel.getNome());
+        pessoaDTO.setEmail(pessoaModel.getEmail());
+        pessoaDTO.setIdade(pessoaModel.getIdade());
+        pessoaDTO.setImgUrl(pessoaModel.getImgUrl());
+        pessoaDTO.setTelefone(pessoaModel.getTelefone());
+        pessoaDTO.setTarefas(pessoaModel.getTarefas());
+
+        return pessoaDTO;
+    }
+
 }
