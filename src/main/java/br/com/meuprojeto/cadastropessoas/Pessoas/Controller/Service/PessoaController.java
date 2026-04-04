7b0f9b650cf73c753bd0bea15bfcb9df96974740
SergioCezar <sergioacezar@hotmail.com>
2026-04-03 22:47:48 -0300

    refactor: refatora o controller para incluir o ResponseEntity nas rotas e customizar a resposta das requisicoes do cliente

--- src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaController.java
+++ src/main/java/br/com/meuprojeto/cadastropessoas/Pessoas/Controller/Service/PessoaController.java
@@ -1,5 +1,7 @@
 package br.com.meuprojeto.cadastropessoas.Pessoas.Controller.Service;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,32 +23,53 @@ public class PessoaController {
 
     //Adicionar pessoa
     @PostMapping("/criar")
-    public PessoaDTO criarPessoa(@RequestBody PessoaDTO pessoa){
-        return pessoaService.criarPessoa(pessoa);
+    public ResponseEntity<String> criarPessoa(@RequestBody PessoaDTO pessoa){
+        PessoaDTO novaPessoa = pessoaService.criarPessoa(pessoa);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Pessoa criada com sucesso: " + novaPessoa.getNome() + "de ID: " + novaPessoa.getId());
     }
 
     //Mostrar todas as pessoas
     @GetMapping("/listar")
-    public List<PessoaDTO> listarPessoas(){
-        return pessoaService.listarPessoas();
+    public ResponseEntity<List<PessoaDTO>> listarPessoas(){
+        return ResponseEntity.ok(pessoaService.listarPessoas());
     }
 
     //Mostrar pessoa por ID
     @GetMapping("/listar/{id}")
     //PathVariable: usa quando a variavel inserida pelo user será parte do caminho. Ex: localhost:8080/listar/1
-    public PessoaDTO listarPessoasPorID(@PathVariable Long id){
-        return pessoaService.listarPessoaPorId(id);
+    public ResponseEntity<?> listarPessoasPorID(@PathVariable Long id){
+
+        if (pessoaService.listarPessoaPorId(id) != null){
+           return ResponseEntity.ok(pessoaService.listarPessoaPorId(id));
+        } else  {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A pessoa com o ID " + id + " não foi encontrada!");
+        }
     }
 
     //Alterar dados das pessoas
     @PutMapping("/alterar/{id}")
-    public PessoaDTO alterarPessoasPorID(@PathVariable Long id, @RequestBody PessoaDTO pessoaAtualizada){
-        return pessoaService.atualizarPessoa(id, pessoaAtualizada);
+    public ResponseEntity<?> alterarPessoasPorID(@PathVariable Long id, @RequestBody PessoaDTO pessoaAtualizada){
+
+        if (pessoaService.listarPessoaPorId(id) != null){
+            return ResponseEntity.ok(pessoaService.atualizarPessoa(id, pessoaAtualizada));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A pessoa com o ID " + id + " não foi encontrada!");
+        }
     }
 
     //Deletar pessoa
     @DeleteMapping("/deletar/{id}")
-    public void deletarPessoaPorID(@PathVariable Long id){
-        pessoaService.deletarPessoaPorId(id);
+    public ResponseEntity<String> deletarPessoaPorID(@PathVariable Long id){
+
+        if(pessoaService.listarPessoaPorId(id) != null) {
+            pessoaService.deletarPessoaPorId(id);
+            return ResponseEntity.ok("Pessoa com ID " + id + " deletada com sucesso!");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("A pessoa com o ID " + id + " não foi encontrada!");
+        }
     }
 }
